6fafb931e0214c5ffbfc7946ad790ebf7f03900e
LeoSle4 <dleochoque4@gmail.com>
2026-09-10 11:33:20 -0500

    Creacion del backend del proyecto

--- Laboratorio-4/Diego_Choque/semana4-api-rest/src/main/java/com/utp/semana4_api_rest/model/Producto.java
+++ Laboratorio-4/Diego_Choque/semana4-api-rest/src/main/java/com/utp/semana4_api_rest/model/Producto.java
@@ -1,60 +1,33 @@
-package com.utp.semana4_api_rest.model;
+package main.java.com.utp.semana4_api_rest.model;
 
-public class Producto {
-    private Long id;
-    private String nombre;
-    private String categoria;
-    private double precio;
-    private int stock;
+import java.time.LocalDateTime;
 
-    public Producto() {
+public class ErrorResponse {
+    private int estado;
+    private String mensaje;
+    private String ruta;
+    private LocalDateTime fechaHora;
+
+    public ErrorResponse(int estado, String mensaje, String ruta) {
+        this.estado = estado;
+        this.mensaje = mensaje;
+        this.ruta = ruta;
+        this.fechaHora = LocalDateTime.now();
     }
 
-    public Producto(Long id, String nombre, String categoria, double precio, int stock) {
-        this.id = id;
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.precio = precio;
-        this.stock = stock;
+    public int getEstado() {
+        return estado;
     }
 
-    public Long getId() {
-        return id;
+    public String getMensaje() {
+        return mensaje;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getRuta() {
+        return ruta;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
     }
 }
