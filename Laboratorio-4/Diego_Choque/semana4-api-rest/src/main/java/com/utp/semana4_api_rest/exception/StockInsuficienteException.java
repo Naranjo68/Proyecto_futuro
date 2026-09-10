7e7e08a50e5cc174fb1faa28de8f3215b57e24ed
LeoSle4 <dleochoque4@gmail.com>
2026-09-10 11:34:21 -0500

    laboratorio 4

--- Laboratorio-4/Diego_Choque/semana4-api-rest/src/main/java/com/utp/semana4_api_rest/exception/StockInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package com.utp.semana4_api_rest.exception;
-
-public class StockInsuficienteException extends RuntimeException {
-    public StockInsuficienteException(String mensaje) {
-        super(mensaje);
-    }
-}
